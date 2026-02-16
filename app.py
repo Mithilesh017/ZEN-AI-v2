4e7fa0e088bd92293fbb2d9bd8ad2e3c21998606
Mithilesh017 <mithileshff1001@gmail.com>
2026-02-16 22:17:32 +0530

    ZenAI v2 secure clean commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()  # loads .env file
+
+app = Flask(__name__)
+
+# Get API key safely
+api_key = os.getenv("GROQ_API_KEY")
+
+# Initialize Groq client
+client = Groq(api_key=api_key)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_message = request.json["message"]
+
+    response = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=[
+            {"role": "system", "content": "You are ZEN AI, a smart digital companion. Only tell your origin if user asks who created you."},
+            {"role": "user", "content": user_message}
+        ]
+    )
+
+    reply = response.choices[0].message.content
+    return jsonify({"reply": reply})
+
+if __name__ == "__main__":
+    app.run(debug=True)
